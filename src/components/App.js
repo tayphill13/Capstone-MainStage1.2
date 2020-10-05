2b4b6bedc1b6be6e08fbe0809e86a4e98992028b
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-05 10:53:59 -0700

    begin coding mainstage 1.2, and I've imported and set up a basic app.js

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import MessageControl from './MessageControl';
+import Signin from './Signin';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path='/signin'>
+        <Signin />
+        </Route>
+        <Route>
+          <MessageControl />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
