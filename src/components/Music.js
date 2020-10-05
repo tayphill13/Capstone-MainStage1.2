d3e447975b153fe020f0376bf7bc469b68a099cb
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-05 14:36:06 -0700

    add to the music page some basic information to check and see if it renders correctly

---
+++ src/components/Music.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Music = () => {
+  return (
+    <div>
+      <h1>This is where all the music will go --- maybe a way to input and then store music to firebase??
+        Or I could try the tutorial for that mp3 player and have a playlist available.
+      </h1>
+    </div>
+  )
+}
+
+export default Music;
