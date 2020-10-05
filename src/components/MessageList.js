ce9a65e782db985608d013052fa74fc4415b9aee
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-05 14:44:15 -0700

    add the messageList

---
+++ src/components/MessageList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Message from './message';
+import { useFirestoreConnect, isLoaded } from "react-redux-firebase";
+import { useSelector } from 'react-redux';
+
+function MessageList(props) {
+  useFirestoreConnect([{ collection: 'messages' }]);
+
+  const messages = useSelector((state) => state.firestore.ordered.messages);
+
+  if (isLoaded(messages)) {
+    return (
+      <React.Fragment>
+        { messages.map((message) => {
+          return (
+            <Message
+                whenMessageClicked={props.onMessageSelection}
+                name={message.name}
+                what={message.what}
+                when={message.when}
+                where={message.where}
+                id={message.id}
+                key={message.key}
+              />
+          );
+        })}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading... just hum along for a moment</h3>
+      </React.Fragment>
+    );
+  }
+}
+MessageList.propTypes = {
+  onMessageSelection: PropTypes.func,
+  message: PropTypes.object,
+};
+
+export default MessageList;
