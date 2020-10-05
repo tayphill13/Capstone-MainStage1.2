34a6462f35303c0284e42887a0fe8c88f665d22c
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-05 11:28:09 -0700

    correct my link on the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
     <div>
-      <h1>Main Stage</h1>
-      <Router>
+        <h1>Main Stage</h1>
         <p>This project began as an idea while I was studying at the Epicodus bootcamp and became and idea for my capstone project.  It will allow artists to provide live shows to their fans without having to fill a venue and especially right now when quarantine rules have got us staying home.  We all love to see/hear live music am I right??</p>
-        <p>You can contact me to give <Route path='/contact'>feedback</Route></p>
-      </Router>
+        <p>You can contact me to give <Link to='/contact'>feedback</Link></p>
     </div>
   )
 }
